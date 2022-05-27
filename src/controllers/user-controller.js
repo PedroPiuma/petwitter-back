6278a0ca3c949fe6dac83b866ef591b8eeab04ce
PedroPiuma <lpap1000@gmail.com>
2022-05-26 22:59:30 -0300

    add new migration; add new seed; add multer; add route to update picture profile

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -11,3 +11,19 @@ export const index = async (req, res) => {
     res.status(500).send({ error: `Cannot fetch users` });
   }
 };
+
+export const updateProfilePicture = async (req, res) => {
+  try {
+    const id = Number(req.params.id)
+    console.log(id)
+    const img = req.file?.path ? req.file.path : ''
+    const newProfile = await prisma.user.update({
+      where: { id },
+      data: { image_url: img }
+    })
+    res.send(newProfile)
+  } catch (error) {
+    console.error("users", error);
+    res.status(500).send('Não foi possível atualizar a foto.');
+  }
+};
--- src/routes/users-routes.js
+++ src/routes/users-routes.js
@@ -1,5 +1,19 @@
 import { validateRequest } from "../middleware/auth.js";
 import * as UserController from "../controllers/user-controller.js";
+import multer from "fastify-multer";
+import path from 'path'
+
+const storage = multer.diskStorage({
+  destination: (req, reply, callback) => {
+    callback(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    const extension = path.extname(file.originalname);
+    cb(null, file.originalname + "-" + Date.now() + extension);
+  },
+});
+
+const upload = multer({ storage });
 
 export default {
   getAllUsers: {
@@ -7,5 +21,11 @@ export default {
     url: "/users",
     preHandler: [validateRequest],
     handler: UserController.index,
+  },
+  updateProfilePicture: {
+    method: "PATCH",
+    url: "/users/:id",
+    preHandler: upload.single("image_url"),
+    handler: UserController.updateProfilePicture,
   },
 };
